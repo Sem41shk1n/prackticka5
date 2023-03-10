466ef4641131d49455108f73601f4187fd017b61
sem41shk1n <ilyxabezyxa@mail.ru>
2023-03-10 23:07:30 +0300

    tortiki

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тортики
+{
+    internal class zakaz
+    {
+        public string[] name = new string[5];
+        public int[] cost = new int[5];
+
+    }
+}
